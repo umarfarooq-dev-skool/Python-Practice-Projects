fc6374811022f2bcb59aa67bafac01dad95090c9
Umar Farooq <umar.farooq.aiskool@gmail.com>
2026-08-18 15:06:38 +0500

    Remove password generation functionality
    Removed the entire password generation logic.

--- Password-Generator.py
+++ Password-Generator.py
@@ -1,13 +1,1 @@
-import random
-import string
 
-length = int(input("Enter password length: "))
-
-characters = string.ascii_letters + string.digits + string.punctuation
-
-password = ""
-
-for i in range(length):
-    password += random.choice(characters)
-
-print("Generated password:", password)
